942dc07b7ad3dcfe693748b6508ccda7fa8277ec
adimalka14 <122941157+adimalka14@users.noreply.github.com>
2024-11-21 13:41:19 +0200

    change console log to logger

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const app = require('./app');
 const connectDb = require('./config/db.config');
+const logger = require('./utils/logger');
+const { PORT } = require('./utils/env-var');
 
 connectDb().then(() =>
-    app.listen(process.env.PORT, () => {
-        console.log(`Server started on port ${process.env.PORT}!`);
+    app.listen(PORT, () => {
+        logger.info(`Server started!`, { PORT });
     })
 );
